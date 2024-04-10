93cc9eca2578f76d8c80ac42564a0cc6da5eb0d6
Aryanmittal23 <mittalaryan932@gmail.com>
2024-04-10 17:37:54 +0530

    passGenerator

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState,useCallback } from 'react'
+import { useState,useCallback ,useEffect,useRef } from 'react'
+import { createDocumentRegistry } from 'typescript';
 
 function App() {
   const [length, setLength] = useState(8)
@@ -8,6 +9,9 @@ function App() {
   (false);
   const [password,setPassword]=useState("")
 
+//useRef hook
+const passwordRef = useRef(null)
+
   const passwordGenerator =useCallback(()=>{
     let pass = ""
     let str = 
@@ -15,32 +19,48 @@ function App() {
     if(numberAllowed) str +="0123456789"
     if(charAllowed) str +="!@#$%^&*-_+={}[]~`"
 
-    for (let i = 0; i <= array.length; i++) {
-      let char =Math.floor(Math.random()*str.length+1)
-      pass =str.charAt(char)
+    for (let i = 1; i <=length; i++) {
+      let char = Math.floor(Math.random()*str.length+1)
+      pass +=str.charAt(char)
       
     }
     setPassword(pass)
 
-  },[length,numberAllowed,charAllowed,password])
+  },[length,numberAllowed,charAllowed,setPassword])
 
-  passwordGenerator(pass)
+const copyPasswordToClipboard = useCallback(()=>{
+  passwordRef.current?.select();
+  passwordRef.current?.setSelectionRange(0,20);
+  window.navigator.clipboard.writeText(password)
+},
+[password])
+
+  useEffect(()=>{
+    passwordGenerator()
+  },[length,numberAllowed,charAllowed,passwordGenerator])
+
+  
   return (
+  
     <>
-      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 
-      my-8 bg-gray-700 text-orange-500 ">
-        <h1 className='text-white text-center my-3'>Password Generator</h1>
-        <div className='className="flex shadow
-        rounded-lg overflow-hidden mb-4"'>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3
+      my-8 bg-gray-800 text-orange-500">
+        <h1 className='text-white text-center my-8'>
+          Password Generator</h1>
+        <div className="flex shadow
+        rounded-lg overflow-hidden mb-4">
           <input
            type="text"
           value={password}
           className='outline-none w-full py-1 px-3'
           placeholder='password'
           readOnly
+          ref={passwordRef}
            />
            <button
-           className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
+           onClick={copyPasswordToClipboard}
+           className='outline-none bg-blue-700 text-white px-3 
+           py-0.5 shrink-0'>copy</button>
         </div>
         <div className='flex text-sm gap-x-2'>
           <div className='flex items-center gap-x-1'>
@@ -77,7 +97,7 @@ function App() {
 
         </div>
         </div>
-    </>
+        </>
   )
 }
 
